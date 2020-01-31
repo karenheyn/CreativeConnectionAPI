50fc14e6af113416f39ba2b245a912db3bea0448
karenheyn <karenheyn@gmail.com>
2020-01-30 22:52:22 -0500

    imported schemas to posts.js

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const router = express.Router();
-//@route GET api/posts
-//@desc test route
-//@access Public
-router.get("/", (req, res) => res.send("Posts Route"));
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
+const Post = require("../../models/Posts");
+const Profile = require("../../models/Profile");
+//@route POST api/posts
+//@desc create post
+//@access Private
+router.post(
+  "/",
+  [auth, [check("text", "Text is required".not().isEmpty())]],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json()[{ errores: errors.array }];
+    }
+
+    const user = await;
+  }
+);
 
 module.exports = router;
