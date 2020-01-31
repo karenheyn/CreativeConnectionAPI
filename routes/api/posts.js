d5adf48889f04e9e4914a32b141ebcec687e327f
karenheyn <karenheyn@gmail.com>
2020-01-31 15:18:53 -0500

    created delete route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -46,4 +46,63 @@ router.post(
   }
 );
 
+// @route    GET api/posts
+// @desc     get all posts
+// @access   Private
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+// @route    GET api/posts/:id
+// @desc     get post by id
+// @access   Private
+
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ message: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+    //check user
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ message: "User not authorized" });
+    }
+    await post.remove();
+    res.json("post removed");
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ message: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route    DELETE api/posts/:id
+// @desc     get post by id
+// @access   Private
+
 module.exports = router;
